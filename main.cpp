6a8f49dca5169d5720055d734f6de07e1ae58376
Elijah Zimmerman <elijahz0409@gmail.com>
2025-04-22 13:27:27 -0400

    Implented command line sml file specification

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include "Computer.h"
 
-int main() {
+int main(int argc, char *argv[]) {
     Computer comp(100);
     std::cout << "Initialized" << std::endl;
-    comp.load("add.sml");
+    comp.load(argv[1]);
     std::cout << "Program read" << std::endl;
     comp.dump();
-
+    comp.run();
+    comp.dump();
     return 0;
 }
