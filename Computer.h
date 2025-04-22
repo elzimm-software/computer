36b70300502122bc529bf758c2681369c5dbe1c4
Elijah Zimmerman <elijahz0409@gmail.com>
2025-04-22 13:26:59 -0400

    Simplified and implemented sml execution

--- Computer.h
+++ Computer.h
@@ -9,18 +9,19 @@
 #include <unistd.h>
 
 
-#define EXIT -99999
-#define MIN_INSTR -9999
-#define MAX_INSTR 9999
+#define EXIT (-99999)
 
 /****Input/output operations: ****/
 #define READ 10 // Read a word from the keyboard into a specific location in memory.
 #define WRITE 11 //Write a word from a specific location in memory to the screen.
+#define WRITEASCIIADDR 12 //Write a character based on the number at the address.
+#define WRITEASCII 13 //Write character defined by the operand. (use the reduced ascii table defined in reduced_ascii_table.csv)
 
 /****Load/store operations: ****/
 #define LOAD 20 //Load a word from a specific location in memory into the accumulator.
 #define STORE 21 //Store a word from the accumulator into a specific location in memory.
-#define DIRECT_LOAD 22 //Load a given word to memory //TODO
+#define DIRECT_LOAD 22 //Load a given word to memory
+#define MOVE 23 //Move the value of a given address to another address.
 
 /****Arithmetic operations:****/
 #define ADD 30 //Add a word from a specific location in memory to the word in the accumulator (leave the result in the accumulator).
@@ -35,145 +36,162 @@
 #define BRANCHZERO 42 //Branch to a specific location in memory if the accumulator is zero.
 #define HALT 43 //Halt—i.e., the program has completed its task.
 
+const char ASCII[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+                       'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+                       '0','1','2','3','4','5','6','7','8','9',
+                       ' ','!','"','#','$','%','&','\'','(',')','*','+',',','-','.','/',
+                       ':',';','<','=','>','?','@','[','\\',']','^','_','`','{','|','}','~',
+                       '\n','\r','\t'};
+
 class Computer {
-    public:
+public:
 
-        Computer(size_t memory_size): memory_size(memory_size), accum(0), instReg(0), instCount(0), opcode(0),operand(0) {
-            memory= new int[memory_size];
-            init_mem();
-        }
+    explicit Computer(size_t memory_size) : memory_size(memory_size), prog_size(0), accum(0), instReg(0), instCount(0), opcode(0), operand(0) {
+        memory = new int[memory_size];
+        init_mem();
+    }
 
-        void dump() const {
-            puts("REGISTERS:");
-            print_registers();
-            puts("MEMORY:");
-            print_memory();
-        }
+    void dump() const {
+        puts("REGISTERS:");
+        print_registers();
+        puts("MEMORY:");
+        print_memory();
+    }
 
-        void load(const std::string& file_name) {
-            std::ifstream input;
-            char cwd[PATH_MAX];
-            get_cur_dir_path(cwd,PATH_MAX);
-            strcat(cwd, file_name.c_str());
+    void load(const std::string& file_name) {
+        std::ifstream input;
 
-            input.open(cwd);
-            if (input.fail()) {
-                input.close();
-                exit(EXIT_FAILURE);
-            }
-            std::string msg;
-            int mem_loc = 0;
-            while (std::getline(input,msg)) {
-                load_command(atoi(msg.c_str()), mem_loc);
-            }
-            puts("+++ Program loading from file completed ***");
+        input.open(file_name);
+        if (input.fail()) {
             input.close();
+            exit(EXIT_FAILURE);
         }
-    private:
-        int* memory;
-        size_t memory_size;
-        //registers
-        int accum; //accumulator register
-        int instReg; //current instruction
-        int instCount; //the number of memory location (0 - 99)
-        int opcode; //operation code
-        int operand; //right most two digits of the operation
-
-        void init_mem() {
-            int i = 0;
-            while (i < memory_size) {
-                memory[i++] = 0;
-            }
+        std::string msg;
+        int mem_loc = 0;
+        while (std::getline(input, msg)) {
+            // atoi call ignores all alpha characters after number
+            load_command(atoi(msg.c_str()), mem_loc++);
         }
+        puts("+++ Program loading from file completed ***");
+        input.close();
+    }
 
-        void print_registers() const {
-            printf("%-20s %+05d\n", "accumulator", accum);
-            printf("%-23s %02d\n", "instructionCounter", instCount);
-            printf("%-20s %+05d\n", "instructionRegister", instReg);
-            printf("%-23s %02d\n", "operationCode", opcode);
-            printf("%-23s %02d\n\n", "operand", operand);
+    int step() {
+        instReg = memory[instCount++];
+        opcode = instReg / 100;
+        operand = (instReg % 100);
+        switch (opcode) {
+            case READ: // read
+                std::cin >> memory[operand+prog_size];
+                break;
+            case WRITE: // write
+                std::cout << memory[operand+prog_size];
+                break;
+            case WRITEASCIIADDR: // write ascii character
+                std::cout << ASCII[memory[operand+prog_size]];
+                break;
+            case WRITEASCII:
+                std::cout << ASCII[operand];
+                break;
+            case LOAD: // load acc
+                accum = memory[operand+prog_size];
+                break;
+            case STORE: // read acc
+                memory[operand+prog_size] = accum;
+                break;
+            case DIRECT_LOAD: // load next operand into operand
+                memory[operand+prog_size] = (memory[instCount++] % 100) + prog_size;
+                break;
+            case MOVE:
+                memory[operand+prog_size] = memory[(memory[instCount++] % 100) + prog_size];
+                break;
+            case ADD: // add
+                accum += memory[operand+prog_size];
+                break;
+            case SUBTRACT: // sub
+                accum -= memory[operand+prog_size];
+                break;
+            case DIVIDE: // div
+                accum /= memory[operand+prog_size];
+                break;
+            case MULTIPLY: // mul
+                accum *= memory[operand+prog_size];
+                break;
+            case REMAINDER:
+                accum %= memory[operand+prog_size];
+                break;
+            case BRANCH: // jmp
+                instCount = operand;
+                break;
+            case BRANCHNEG: // jmp if neg
+                instCount = operand ? accum < 0 : instCount;
+                break;
+            case BRANCHZERO: // jmp if zero
+                instCount = operand ? accum == 0 : instCount;
+                break;
+            case HALT: // halt
+                return HALT;
         }
+        return 0;
+    }
 
-        void print_memory() const {
-            printf("%9d", 0);
-            for(int i = 1; i < 10; i++) {
-                printf("%7d", i);
-            }
-            putchar('\n');
-            int row_index = 0;
-            printf("%2d", row_index);
-            for(int i = 0; i < memory_size; i++) {
-                if (i > 0 && i % 10 == 0) {
-                    putchar('\n');
-                    printf("%2d", row_index += 10);
-                }
-                printf("%+7.4d", memory[i]);
-            }
-            putchar('\n');
-        }
+    int run() {
+        while (!step());
+        return 0;
+    }
 
-        void get_cur_dir_path(char* cwd, size_t size) const {
-            if (getcwd(cwd, size) != NULL) {
-                strcat(cwd,"/");
-            } else {
-                perror("getcwd() error");
-                exit(EXIT_FAILURE);
-            }
-        }
+private:
+    int* memory;
+    size_t memory_size;
+    int prog_size;
+    //registers
+    int accum; //accumulator register
+    int instReg; //current instruction
+    int instCount; //the number of memory location (0 - 99)
+    int opcode; //operation code
+    int operand; //right most two digits of the operation
 
-        void load_command(int instr, int memory_location) {
-            if (instr == EXIT || (instr <= MIN_INSTR && instr >= MAX_INSTR)) {
-                return;
-            }
-            memory[memory_location] = instr;
-        }
-
-    void run() {
-        int op = 00;
-        int addr = 00;
-        while (op != 43) {
-            switch (op) {
-                case 10: // read
-
-                    break;
-                case 11: // write
-
-                    break;
-                case 20: // load acc
-
-                    break;
-                case 21: // read acc
-
-                    break;
-                case 30: // add
-
-                    break;
-                case 31: // sub
-
-                    break;
-                case 32: // div
-
-                    break;
-                case 33: // mul
-
-                    break;
-                case 40: // jmp
-
-                    break;
-                case 41: // jmp if neg
-
-                    break;
-                case 42: // jmp if zero
-
-                    break;
-                case 43: // halt
-
-                    break;
-                default:
-                    break;
-            }
+    void init_mem() {
+        int i = 0;
+        while (i < memory_size) {
+            memory[i++] = 0;
         }
     }
+
+    void print_registers() const {
+        printf("%-20s %+05d\n", "accumulator", accum);
+        printf("%-23s %02d\n", "instructionCounter", instCount);
+        printf("%-20s %+05d\n", "instructionRegister", instReg);
+        printf("%-23s %02d\n", "operationCode", opcode);
+        printf("%-23s %02d\n\n", "operand", operand);
+    }
+
+    void print_memory() const {
+        printf("%9d", 0);
+        for (int i = 1; i < 10; i++) {
+            printf("%7d", i);
+        }
+        putchar('\n');
+        int row_index = 0;
+        printf("%2d", row_index);
+        for (int i = 0; i < memory_size; i++) {
+            if (i > 0 && i % 10 == 0) {
+                putchar('\n');
+                printf("%2d", row_index += 10);
+            }
+            printf("%+7.4d", memory[i]);
+        }
+        putchar('\n');
+    }
+
+    void load_command(int instr, int memory_location) {
+        if (instr == EXIT) {
+            return;
+        }
+        memory[memory_location] = instr;
+        prog_size++;
+    }
+
 };
 
 #endif
